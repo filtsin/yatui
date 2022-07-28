6156ed1f4265315ea313de653d7d3ebf71160bf9
Ilya Filtsin <ilfiltsin@gmail.com>
2022-07-28 22:28:06 +0300

    Add Text tests

--- src/terminal/text.rs
+++ src/terminal/text.rs
@@ -1,9 +1,15 @@
 use crate::terminal::style::Style;
+use smallvec::SmallVec;
 use std::borrow::Cow;
 
+#[derive(Clone)]
 pub struct Str {
     content: Cow<'static, str>,
     style: Style,
+}
+
+pub struct Text {
+    content: SmallVec<[Str; 2]>,
 }
 
 impl Str {
@@ -17,6 +23,27 @@ impl Str {
 
     pub fn is_owned(&self) -> bool {
         matches!(self.content, Cow::Owned(_))
+    }
+
+    pub fn styles(&self) -> Style {
+        self.style
+    }
+
+    pub fn styles_mut(&mut self) -> &mut Style {
+        &mut self.style
+    }
+}
+
+impl Text {
+    pub fn new<I>(content: I) -> Self
+    where
+        I: IntoIterator<Item = Str>,
+    {
+        Self { content: SmallVec::from_iter(content.into_iter()) }
+    }
+
+    pub fn spilled(&self) -> bool {
+        self.content.spilled()
     }
 }
 
--- tests/text.rs
+++ tests/text.rs
@@ -1,10 +1,47 @@
-use yatui::terminal::text::Str;
+use yatui::terminal::{
+    style::{Color, Style},
+    text::{Str, Text},
+};
+
+use pretty_assertions::assert_eq;
 
 #[test]
-fn create() {
+fn create_borrowed_and_owned_strings() {
     let str: Str = "hello".into();
     let string: Str = "hello".to_owned().into();
 
     assert!(str.is_borrowed());
     assert!(string.is_owned());
+}
+
+#[test]
+fn change_styles_mut() {
+    let mut str: Str = "hello".into();
+
+    assert_eq!(str.styles(), Style::default());
+
+    str.styles_mut().fg(Color::Red);
+
+    assert_eq!(str.styles(), Style::new().fg(Color::Red));
+}
+
+#[test]
+fn create_text_with_2part_of_str_should_not_allocate() {
+    let str: Str = "hello".into();
+    let str2 = str.clone();
+
+    let text = Text::new([str, str2]);
+
+    assert!(!text.spilled());
+}
+
+#[test]
+fn create_text_with_more_than_2part_of_str_should_not_allocate() {
+    let str: Str = "hello".into();
+    let str2 = str.clone();
+    let str3 = str.clone();
+
+    let text = Text::new([str, str2, str3]);
+
+    assert!(text.spilled());
 }
